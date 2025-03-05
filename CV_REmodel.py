896053d207870c2a731425c05271b0c1221b5839
Xemin0 <shilai_yang@brown.edu>
2025-03-05 07:43:38 -0700

    minorfix: fixed potential importing issues; fixed mispellings of a couple vars in non-`.ipynb` files

--- CV_REmodel.py
+++ CV_REmodel.py
@@ -1,5 +1,9 @@
 import torch
 
+# For RandSeed
+import numpy as np
+import random
+
 from data.data_loader import load_data
 from model.reading_embedding_model import ReadingEmbeddingModel
 from utils.cross_validation import k_fold_CV
@@ -19,7 +23,7 @@ Load Real Data
 
 ### Load the data of of i-th subject
 idx = 0 #### 0 - 8 for 9 different subjects
-feat_choice = [0, 1, 0]
+feat_choice = [0, 1, 0] # An indicator list/vector representing the choice of  [word_embedding, eeg_feats, eye_gaze_feats]
 epochs = 500
 n_translayer = 1
 opt_type = 'SGD' # or 'Adam'
@@ -44,10 +48,12 @@ embeddings, eeg_features, gaze_features, labels, sen_len = original_data
 # Device setup
 '''
 seed = 42
+random.seed(seed)
+np.random.seed(seed)
 torch.manual_seed(seed)
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 # seed gpus if available
-if torch.cuda.isavalable():
+if torch.cuda.is_available():
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
 print(device)
--- trainREmodel.py
+++ trainREmodel.py
@@ -1,5 +1,9 @@
 import torch
 
+## For RandSeed
+import numpy as np
+import random
+
 from data.data_loader import load_data
 from model.reading_embedding_model import ReadingEmbeddingModel
 from utils.train_test import train_model
@@ -19,7 +23,7 @@ Load Real Data
 
 ### Load the data of of i-th subject
 idx = 0 #### 0 - 8 for 9 different subjects
-feat_choice = [0, 1, 0]
+feat_choice = [0, 1, 0]  # An indicator list/vector representing the choice of  [word_embedding, eeg_feats, eye_gaze_feats]
 epochs = 480
 opt_type = 'SGD' # Adam
 
@@ -41,10 +45,12 @@ embeddings, eeg_features, gaze_features, labels, sen_len = original_data
 # Device setup
 '''
 seed = 42
+random.seed(seed)
+np.random.seed(seed)
 torch.manual_seed(seed)
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 # seed gpus if available
-if torch.cuda.isavalable():
+if torch.cuda.is_available():
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
 print(device)
@@ -82,12 +88,12 @@ re_no_pad = torch.cat([sen[:l] for sen, l in zip(reading_emb, d_sen_len)], dim =
 lb_no_pad = torch.cat([sen[:l] for sen, l in zip(d_labels, d_sen_len)], dim = 0)
 
 
-if 2 == sum(f_choice):
+if 2 == sum(feat_choice):
     f_type = 'EEG + Eye_Gaze'
-elif 1 == sum(f_choice):
-    if f_choice[1]:
+elif 1 == sum(feat_choice):
+    if feat_choice[1]:
         f_type = 'EEG'
-    elif f_choice[2]:
+    elif feat_choice[2]:
         f_type = 'EyeGaze'
     else:
         f_type = ' '
